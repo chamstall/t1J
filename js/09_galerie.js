136a1bc6fdb347cf5f5c7cf0e9622ad99ff16bf9
Chams <tallbachir80@gmail.com>
2023-06-12 23:19:49 +0000

    ajoutssssss

--- js/09_galerie.js
+++ js/09_galerie.js
@@ -1,6 +1,7 @@
 const images = document.querySelectorAll("#galery img");
 
 let imgActive = 0;
+let interval ; // variable globale pour stocker le temps
 images[imgActive].classList.add("show");
 
 // masquer les photos sauf la 1ere
@@ -11,6 +12,20 @@ for (let i = 1; i < images.length; i += 1) {
 // clic sur le bouton suivant
 document.querySelector("#next").addEventListener("click", function () {
     next();
+});
+// clic sur le bouton precedent
+document.querySelector("#prev").addEventListener("click",function(){
+    prev();
+})
+
+// clic sur le bouton play
+document.querySelector("#play").addEventListener("click",function(){
+    interval = setInterval(next ,2000 ) //creation de l'interval de temps
+});
+
+// clic sur le bouton pause
+document.querySelector("#pause").addEventListener("click",function(){
+    clearInterval(interval);  //destruction de l'intervalle de temps
 });
 
 // FONCTION POUR PHOTO suivante
@@ -29,11 +44,6 @@ const next = function(){
 };
 
 
-// clic sur le bouton precedent
-document.querySelector("#prev").addEventListener("click",function(){
-    prev();
-})
-
 // FONCTION POUR PHOTO suivante
 const prev = function(){
     images[imgActive].classList.add("hidden");
@@ -47,15 +57,17 @@ const prev = function(){
         images[imgActive].classList.add("show");
     },200);
 
-}
+};
 
-// clic sur le bouton play
-let interval ;
-document.querySelector("#play").addEventListener("click",function(){
-    interval = setInterval(next ,2000 )
-})
+//Gestion touche du clavier
+window.addEventListener("keydown" , function(e){
+    if(e.key == "ArrowRight"){
+        next();
+    }
+    if(e.key == "ArrowLeft"){
+        prev();
+    }
+});
 
-// clic sur le bouton pause
-document.querySelector("#pause").addEventListener("click",function(){
-    clearInterval(interval);
-})
+
+
